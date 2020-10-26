ff6bc211fa970564557a7983c8ab7045452a575a
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-10-27 00:56:18 +0530

    interfaces and ngOnInit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User'
 
 @Component({
   selector: 'app-user',
@@ -12,64 +13,25 @@ import { Component } from '@angular/core';
   // `]
 })
 
-export class UserComponent{
+export class UserComponent implements OnInit{
   //properties
-  firstName:string;
-  lastName:string;
-  age:number;
-  address;
-  foo:any;
-  hasKids:boolean;
-  numberArray:number[];
-  stringArray:string[];
-  mixedArray:any[];
-  myTuple: [string,number,boolean];
-  unusable:void;
-  u:undefined;
-  a:null;
+  user: User;
 
   //methods
   constructor(){
-    //console.log('Hello User...')
-    // this.sayHello();
-    // console.log(this.age);
-    // this.hasBirthday();
-    // console.log(this.age)
-    this.firstName='Jon';
-    this.lastName='Doe';
-    this.age=25;
-    this.address = {
-      street: '20th cross',
-      city:'Bengaluru',
-      state:'Karnataka'
-    };
-    this.foo = true;
-    this.hasKids = true;
-    this.numberArray = [1,2,3];
-    this.stringArray = ['hello','world'];
-    this.mixedArray = [true, undefined, 'hello'];
-    this.myTuple = ['hello', 1, true];
-    this.unusable= undefined;
-    this.u = undefined;
-    this.a = null;
+  }
 
-    console.log(this.addNumbers(5, 20));
+  ngOnInit(){
+    this.user = {
+      firstName:'Jon',
+      lastName:'Doe',
+      age:25,
+      address:{
+        street:'10th cross',
+        city:'Bengaluru',
+        state:'Karnataka'
+      }
+    }   
   }
   
-  sayHello(){
-    console.log(`Hello ${this.firstName}`)
-    console.log('Hello ' + this.lastName)
-  }
-
-  hasBirthday(){
-    this.age += 1
-  }
-
-  showAge(){
-    return this.age;
-  }
-
-  addNumbers(num1:number, num2:number):number{
-    return num1 + num2;
-  }
 }
