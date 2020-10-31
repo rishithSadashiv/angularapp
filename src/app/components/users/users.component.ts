c5b2a3fad22c975ee7cbed678013cc1c43697c2c
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-10-31 15:49:35 +0530

    ngIf

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,55 +8,52 @@ import { User } from 'src/app/models/User';
 })
 export class UsersComponent implements OnInit {
   users:User[];
+  showExtended:boolean = true;
+  loaded:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.users = [
-      {
-        firstName:'Jon',
-        lastName:'Doe',
-        age:25,
-        address:{
-          street:'10th cross',
-          city:'Bengaluru',
-          state:'Karnataka'
-        }
-      },
-      {
-        firstName:'Na',
-        lastName:'Mo',
-        age:55,
-        address:{
-          street:'8th cross',
-          city:'Mysuru',
-          state:'Karnataka'
-        }
-      },
-      {
-        firstName:'Ra',
-        lastName:'Ga',
-        age:45,
-        address:{
-          street:'7th cross',
-          city:'Tumakuru',
-          state:'Karnataka'
-        }
-      }
-    ];
 
-    this.addUser(
-      {
-      firstName:'Am',
-      lastName:'Sh',
-      age:50,
-      address:{
-        street:'6th cross',
-        city:'Mangaluru',
-        state:'Karnataka'
+      this.users = [
+        {
+          firstName:'Jon',
+          lastName:'Doe',
+          age:25,
+          address:{
+            street:'10th cross',
+            city:'Bengaluru',
+            state:'Karnataka'
+          }
+        },
+        {
+          firstName:'Na',
+          lastName:'Mo',
+          age:55,
+          address:{
+            street:'8th cross',
+            city:'Mysuru',
+            state:'Karnataka'
+          }
+        },
+        {
+          firstName:'Ra',
+          lastName:'Ga',
+          age:45,
+          address:{
+            street:'7th cross',
+            city:'Tumakuru',
+            state:'Karnataka'
+          }
         }
-      }
-    );
+      ];
+      this.loaded = true;
+    
+    
+    this.addUser({
+      firstName:'Am',
+      lastName:'Sh'
+    });
 
   }
 
