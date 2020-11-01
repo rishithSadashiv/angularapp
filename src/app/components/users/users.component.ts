47c09cda70d991dc3961a882b3b6137bc61c5e2b
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-11-02 01:45:12 +0530

    modifications

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -10,12 +10,7 @@ export class UsersComponent implements OnInit {
   user:User={
     firstName:'',
     lastName:'',
-    age:null,
-    address:{
-      street:'',
-      city:'',
-      state:''
-    }
+    email:''
   };
   users:User[];
   showExtended:boolean = true;
@@ -31,12 +26,7 @@ export class UsersComponent implements OnInit {
         {
           firstName:'Jane',
           lastName:'Doe',
-          age:25,
-          address:{
-            street:'10th cross',
-            city:'Bengaluru',
-            state:'Karnataka'
-          },
+          email:'jane@hotmail.com',
           //lorempixel is a website which generates images (200,200 is dimension of the image, people is the category of the image)
           isActive:true,
           registered: new Date('01/11/2020 00:49:40 '),
@@ -45,12 +35,7 @@ export class UsersComponent implements OnInit {
         {
           firstName:'Na',
           lastName:'Mo',
-          age:55,
-          address:{
-            street:'8th cross',
-            city:'Mysuru',
-            state:'Karnataka'
-          },
+          email:'namo@gmail.com',
           isActive:false,
           registered: new Date('01/10/2020 10:49:40 '),
           hide: true
@@ -59,12 +44,7 @@ export class UsersComponent implements OnInit {
         {
           firstName:'Ra',
           lastName:'Ga',
-          age:45,
-          address:{
-            street:'7th cross',
-            city:'Tumakuru',
-            state:'Karnataka'
-          },
+          email:'raga@twitter.com',
           isActive:true,
           registered: new Date('08/08/2020 09:07:00'),
           hide: true
@@ -73,37 +53,25 @@ export class UsersComponent implements OnInit {
       this.loaded = true;
   }
 
-  addUser(){
-    // console.log(this.users.push(this.user))
-    // unshift is same as push but it adds the new element to the front of the array
-    this.user.isActive = true;
-    this.user.registered = new Date();
-    this.users.unshift(this.user);
-    this.user = {
-      firstName:'',
-      lastName:'',
-      age:null,
-      address:{
-        street:'',
-        city:'',
-        state:''
-      }
-    }
-    
-  }
-
-  // toggleHide(user:User){
-  //   user.hide = !user.hide;
+  // addUser(){
+  //   // console.log(this.users.push(this.user))
+  //   // unshift is same as push but it adds the new element to the front of the array
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+  //   this.users.unshift(this.user);
+  //   this.user = {
+  //     firstName:'',
+  //     lastName:'',
+  //     email:''
+  //   }
   // }
+
 
   onSubmit(e){
     console.log(123);
     e.preventDefault();
   }
 
-  fireEvent(e){
-    console.log(e.target.value);
-    console.log(e.type);
-  }
+  
 
 }
